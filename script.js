358fad2273635321f85a2a1b9f0f221e0ba4ccce
gigapapakerashvili <giga.papakerashvili@mail.ru>
2021-06-11 14:22:28 +0400

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("nav-right");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myClickFunction() {
+    alert('search item');
+}
+
